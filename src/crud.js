11abefeaba93b62edc7d2ccc1c9bc31297ab3c13
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-28 17:37:08 -0500

    Move populateList function to avoid circular import

--- src/crud.js
+++ src/crud.js
@@ -1,7 +1,84 @@
 import ToDo from './todoList';
-import { populateList } from './index';
+import DeleteButton from './delete-btn.png';
 
-// Add
+// Add items to UI
+export function populateList() {
+  const todoList = document.getElementById('todo-list');
+  todoList.innerHTML = '';
+
+  ToDo.list.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('id', item.index);
+    listItem.classList = 'item-container';
+
+    listItem.innerHTML = `
+    <input class="checkbox" type="checkbox">
+    <span>${item.description}</span>
+    <textarea class="text-area" maxlength="30">${item.description}</textarea>
+    <img class="cancel-btn" src="${DeleteButton}">
+    `;
+
+    todoList.appendChild(listItem);
+
+    const checkbox = listItem.querySelector('input');
+    const text = listItem.querySelector('span');
+    const textInput = listItem.querySelector('textarea');
+    const deleteButton = listItem.querySelector('img');
+
+    // Update
+    checkbox.addEventListener('change', () => {
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].update();
+      text.classList.toggle('complete');
+      textInput.classList.toggle('complete');
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+    });
+
+    // Edit functionality
+    text.addEventListener('click', () => {
+      text.style.display = 'none';
+      textInput.classList.toggle('edit-item');
+    });
+
+    textInput.addEventListener('keydown', (e) => {
+      // Follow value
+      text.innerHTML = textInput.value;
+
+      // Update list
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].description = text.innerHTML;
+
+      // Update local storage
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+
+      if (e.code === 'Enter') {
+        // Update UI
+        text.style.display = 'block';
+        textInput.classList.toggle('edit-item');
+      }
+    });
+
+    // Delete functionality
+    deleteButton.addEventListener('click', () => {
+      // Update list
+      const index = parseInt(listItem.id, 10);
+      ToDo.list = ToDo.list.filter((item) => item !== ToDo.list[index]);
+      // Update indexes
+      ToDo.list.forEach((item, i) => { item.index = i; });
+      // Update local storage
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+      // Update UI
+      populateList();
+    });
+
+    if (item.complete) {
+      checkbox.checked = true;
+      text.classList = 'complete';
+    }
+  });
+}
+
+// Add functionality
 export function add(e) {
   if (e.code === 'Enter') {
     // Create new item
@@ -16,38 +93,7 @@ export function add(e) {
   }
 }
 
-// Edit
-export function edit(e) {
-  // Follow value
-  this.previousElementSibling.innerHTML = this.value;
-
-  // Update list
-  const index = parseInt(this.parentNode.id, 10);
-  ToDo.list[index].description = this.previousElementSibling.innerHTML;
-
-  // Update local storage
-  localStorage.setItem('todoList', JSON.stringify(ToDo.list));
-
-  if (e.code === 'Enter') {
-    // Update UI
-    this.previousElementSibling.style.display = 'block';
-    this.classList.toggle('edit-item');
-  }
-}
-
-// Delete
-export function deleteItem() {
-  // Update list
-  const index = parseInt(this.parentNode.id, 10);
-  ToDo.list = ToDo.list.filter((item) => item !== ToDo.list[index]);
-  // Update indexes
-  ToDo.list.forEach((item, i) => { item.index = i; });
-  // Update local storage
-  localStorage.setItem('todoList', JSON.stringify(ToDo.list));
-  // Update UI
-  populateList();
-}
-
+// Clear completed functionality
 export function deleteAllCompleted() {
   // Update list
   ToDo.list = ToDo.list.filter((item) => item.complete === false);
--- src/index.js
+++ src/index.js
@@ -1,68 +1,11 @@
 import './style.css';
-import {
-  add, edit, deleteItem, deleteAllCompleted,
-} from './crud';
+import { add, deleteAllCompleted, populateList } from './crud';
 import ToDo from './todoList';
-import DeleteButton from './delete-btn.png';
-
-const defaultList = ['Finish project', 'Groceries', 'Fix laptop'];
-
-// Add items to UI
-export function populateList() {
-  const todoList = document.getElementById('todo-list');
-  todoList.innerHTML = '';
-
-  ToDo.list.forEach((item) => {
-    const listItem = document.createElement('li');
-    listItem.setAttribute('id', item.index);
-    listItem.classList = 'item-container';
-
-    listItem.innerHTML = `
-    <input class="checkbox" type="checkbox">
-    <span>${item.description}</span>
-    <textarea class="text-area" maxlength="30">${item.description}</textarea>
-    <img class="cancel-btn" src="${DeleteButton}">
-    `;
-
-    todoList.appendChild(listItem);
-
-    const checkbox = listItem.querySelector('input');
-    const text = listItem.querySelector('span');
-    const textInput = listItem.querySelector('textarea');
-    const deleteButton = listItem.querySelector('img');
-
-    // Update
-    checkbox.addEventListener('change', () => {
-      const index = parseInt(listItem.id, 10);
-      ToDo.list[index].update();
-      text.classList.toggle('complete');
-      textInput.classList.toggle('complete');
-      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
-    });
-
-    // Edit
-    text.addEventListener('click', () => {
-      text.style.display = 'none';
-      textInput.classList.toggle('edit-item');
-    });
-    textInput.addEventListener('keydown', edit);
-
-    // Delete
-    deleteButton.addEventListener('click', deleteItem);
-
-    if (item.complete) {
-      checkbox.checked = true;
-      text.classList = 'complete';
-    }
-  });
-}
 
 // Window load
 const list = JSON.parse(localStorage.getItem('todoList'));
 if (list) {
   list.forEach((item) => new ToDo(item.description, item.complete));
-} else {
-  defaultList.forEach((item) => new ToDo(item, false));
 }
 
 // Add
