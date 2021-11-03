fcf5741f130434d6b5a7f65fd0efc1acd36a0280
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-03 17:02:23 -0500

    Add more test files

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,6 @@ import { add } from '../src/crud';
 
 describe('Tests for todo list app', () => {
   test('should add item to the list', () => {
-    const e = {
-      code: 'Enter'
-    }
-
-    expect(add(e)).toBe(1)
-
+    expect(add("Some text")).toBe(1)
   })
 })
