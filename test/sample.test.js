7414466825b7a8e5d91cdde9dee4ef7db10e0170
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-11-03 15:47:18 -0500

    Add test for add function

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,17 +1,12 @@
-import { deleteAllCompleted } from '../src/crud';
+import { add } from '../src/crud';
 
 describe('Tests for todo list app', () => {
-  // test('should add item to the list', () => {
-    
-  // })
-  
-  // test('should remove item from the list', () => {
-    
-  // })
-  
-  test('should delete all items completed', () => {
-    // const deleteAllCompleted = jest.fn();
-    expect(deleteAllCompleted()).toBeDefined();
+  test('should add item to the list', () => {
+    const e = {
+      code: 'Enter'
+    }
+
+    expect(add(e)).toBe(1)
+
   })
-  
 })
